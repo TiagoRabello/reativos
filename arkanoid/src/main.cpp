09d7daadfba0808d6198c8808ccc31a0727bb5dd
TiagoRabello <tiago.gomes_mtg@hotmail.com>
2015-04-17 11:52:03 -0300

    Adicionada a possibilidade de dar reset no arkanoid.

--- arkanoid/src/main.cpp
+++ arkanoid/src/main.cpp
@@ -10,7 +10,7 @@
 
 const SDL_Rect game_area = { 120, 0, 400, 480 };
 const auto move_speed = 500;
-auto player_lifes = 3;
+
 template<typename T> T clamp(T value, T min, T max)
 {
   return std::max(min, std::min(max, value));
@@ -137,6 +137,133 @@ void process_collision(ball &b, vec2 delta_position, double t, vec2 normal)
 
   b.shape.position = b.shape.position + (delta_position * remaining_time);
 }
+
+enum class program_state { in_game, menu };
+
+program_state                state;
+int                          player_lives;
+player_pallet                player;
+ball                         b;
+std::vector<block>           blocks;
+std::shared_ptr<SDL_Texture> player_texture;
+
+ball create_ball(std::shared_ptr<SDL_Texture> texture)
+{
+  return{ { { game_area.x + game_area.w / 2.0 + 5, game_area.y + game_area.h - 20.0 }, 10, 10 }, { 100, -100 }, { 5, -5 }, std::move(texture), { 0, 0, 32, 8 } };
+}
+void init_game_state(SDL_Renderer *renderer)
+{
+  player_lives = 3;
+
+  if (player_texture == nullptr)
+  {
+    player_texture = load_texture(renderer, "assets/player_sprite.bmp");
+  }
+
+  blocks = make_blocks(renderer, { game_area.x + 10, game_area.y + 10, game_area.w - 20, (game_area.h - 10) / 4 }, 10, 6, 0);
+
+  player.shape = { { game_area.x + 2.0 * game_area.w / 5, game_area.y + game_area.h - 22.0 }, 78, 18 };
+  player.velocity = { 0.0, 0.0 };
+  player.acceleration = { 0.0, 0.0 };
+  player.texture = player_texture;
+  player.texture_area = { 0, 0, 52, 12 };
+
+  b = create_ball(player_texture);
+      }
+void process_in_game_events(const SDL_Event& event, SDL_Renderer *)
+{
+  if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
+  {
+    if (event.key.repeat == false)
+    {
+      if (event.key.keysym.sym == SDLK_LEFT) { player.velocity.x += (event.type == SDL_KEYDOWN) ? -move_speed : move_speed; }
+      else if (event.key.keysym.sym == SDLK_RIGHT) { player.velocity.x += (event.type == SDL_KEYDOWN) ? move_speed : -move_speed; }
+    }
+  }
+}
+void process_menu_events(const SDL_Event& event, SDL_Renderer *renderer)
+{
+  if (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_SPACE)
+  {
+    init_game_state(renderer);
+    state = program_state::in_game;
+  }
+}
+void in_game_loop(std::chrono::milliseconds elapsed, SDL_Renderer *renderer)
+{
+    const auto ball_prev = b.shape;
+    player.update(elapsed);
+    b.update(elapsed);
+    auto delta_position = b.shape.position - ball_prev.position;
+
+  if (b.shape.position.x > game_area.w + game_area.x || b.shape.position.x < game_area.x)
+  {
+      b.shape.position.x = clamp(b.shape.position.x, (double)game_area.x, (double)game_area.w + game_area.x);
+      b.velocity.x = -b.velocity.x;
+      b.acceleration.x = -b.acceleration.x;
+    }
+  if (b.shape.position.y > game_area.h + game_area.y)
+  {
+    --player_lives;
+    b = create_ball(player_texture);
+    }
+  if (b.shape.position.y < game_area.y)
+  {
+      b.shape.position.y = game_area.y;
+      b.velocity.y = -b.velocity.y;
+      b.acceleration.y = -b.acceleration.y;
+    }
+
+    vec2 closest_normal = {};
+    double closest_t = 1.0;
+    block *closest_block = nullptr;
+  for (auto&& block : blocks)
+  {
+      auto t = sweep_aabb(ball_prev, block.shape, delta_position * closest_t, closest_normal);
+    if (t < closest_t)
+    {
+        closest_t = t;
+        closest_block = &block;
+      }
+    }
+    {
+      auto t = sweep_aabb(ball_prev, player.shape, delta_position * closest_t, closest_normal);
+    if (t < closest_t)
+    {
+        closest_t = t;
+        closest_block = nullptr;
+      }
+    }
+  if (closest_t != 1.0)
+  {
+      process_collision(b, delta_position, closest_t, closest_normal);
+      if (closest_block != nullptr) { --(closest_block->durability); }
+    }
+
+    auto new_end = std::remove_if(std::begin(blocks), std::end(blocks), [](const block& b) { return b.durability == 0; });
+    blocks.erase(new_end, std::end(blocks));
+
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xBB, SDL_ALPHA_OPAQUE);
+  SDL_RenderFillRect(renderer, &game_area);
+
+  for (auto&& block : blocks) { block.draw(renderer); }
+
+  player.draw(renderer);
+  b.draw(renderer);
+
+  SDL_RenderPresent(renderer);
+
+  if (blocks.empty() || player_lives == 0)
+  {
+    std::cout << ((player_lives == 0) ? "Voce perdeu... =(" : "Voce ganhou! =)))") << std::endl;
+    std::cout << "Pressione espaco para recomecar o jogo." << std::endl;
+    state = program_state::menu;
+  }
+}
+void menu_loop(std::chrono::milliseconds, SDL_Renderer *) { }
+
 int main(int, char *[])
 {
   if (SDL_Init(SDL_INIT_EVERYTHING)) { return -1; }
@@ -150,98 +277,48 @@ int main(int, char *[])
   auto renderer = std::unique_ptr<SDL_Renderer, void(*)(SDL_Renderer*)>{ SDL_CreateRenderer(window.get(), -1, 0), SDL_DestroyRenderer };
   if (renderer == nullptr) { return 2; }
 
-  auto player_texture = load_texture(renderer.get(), "assets/player_sprite.bmp");
+  init_game_state(renderer.get());
+  state = program_state::in_game;
 
-  auto blocks = make_blocks(renderer.get(), { game_area.x + 10, game_area.y + 10, game_area.w - 20, (game_area.h - 10) / 4 }, 10, 6, 0);
-  player_pallet player{};
-  player.shape = { { game_area.x + 2.0 * game_area.w / 5, game_area.y + game_area.h - 22.0 }, 78, 18 };
-  player.velocity = { 0.0, 0.0 };
-  player.acceleration = { 0.0, 0.0 };
-  player.texture = player_texture;
-  player.texture_area = { 0, 0, 52, 12 };
-
-  ball b = { { { game_area.x + game_area.w / 2.0 + 5, game_area.y + game_area.h - 20.0 }, 10, 10 }, { 100, -100 }, { 5, -5 }, player_texture, { 0, 0, 32, 8 } };
-  const auto original_ball = b;
-
-  auto prev_time = std::chrono::high_resolution_clock::now();
+  using clock = std::chrono::high_resolution_clock;
+  auto prev_time = clock::now();
 
   bool must_close = false;
-  while (must_close == false) {
+  while (must_close == false)
+  {
     SDL_Event e;
-    while (SDL_PollEvent(&e)) {
+    if (SDL_PollEvent(&e))
+    {
       if (e.type == SDL_QUIT) { must_close = true; }
-      else if (e.type == SDL_KEYDOWN || e.type == SDL_KEYUP) {
-        if (e.key.repeat) { continue; }
-
-             if (e.key.keysym.sym == SDLK_LEFT) { player.velocity.x += (e.type == SDL_KEYDOWN) ? -move_speed : move_speed; }
-        else if (e.key.keysym.sym == SDLK_RIGHT) { player.velocity.x += (e.type == SDL_KEYDOWN) ? move_speed : -move_speed; }
+      else
+      {
+        switch (state)
+        {
+          case program_state::in_game:
+            process_in_game_events(e, renderer.get());
+            break;
+          case program_state::menu:
+            process_menu_events(e, renderer.get());
+            break;
+        }
       }
     }
 
-    if (blocks.empty()) { std::cout << "Voce venceu! =)" << std::endl; break; }
-    if (player_lifes == 0) { std::cout << "Voce perdeu... =(" << std::endl; break; }
-
-    const auto curr_time = std::chrono::high_resolution_clock::now();
+    const auto curr_time = clock::now();
     const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(curr_time - prev_time);
     prev_time = curr_time;
 
-    const auto ball_prev = b.shape;
-    player.update(elapsed);
-    b.update(elapsed);
-    auto delta_position = b.shape.position - ball_prev.position;
-
-    if (b.shape.position.x > game_area.w + game_area.x || b.shape.position.x < game_area.x) {
-      b.shape.position.x = clamp(b.shape.position.x, (double)game_area.x, (double)game_area.w + game_area.x);
-      b.velocity.x = -b.velocity.x;
-      b.acceleration.x = -b.acceleration.x;
-    }
-    if (b.shape.position.y > game_area.h + game_area.y) {
-      --player_lifes;
-      b = original_ball;
-    }
-    if (b.shape.position.y < game_area.y) {
-      b.shape.position.y = game_area.y;
-      b.velocity.y = -b.velocity.y;
-      b.acceleration.y = -b.acceleration.y;
-    }
-
-    vec2 closest_normal = {};
-    double closest_t = 1.0;
-    block *closest_block = nullptr;
-    for (auto&& block : blocks) {
-      auto t = sweep_aabb(ball_prev, block.shape, delta_position * closest_t, closest_normal);
-      if (t < closest_t) {
-        closest_t = t;
-        closest_block = &block;
-      }
-    }
+    switch (state)
     {
-      auto t = sweep_aabb(ball_prev, player.shape, delta_position * closest_t, closest_normal);
-      if (t < closest_t) {
-        closest_t = t;
-        closest_block = nullptr;
-      }
+      case program_state::in_game:
+        in_game_loop(elapsed, renderer.get());
+        break;
+      case program_state::menu:
+        menu_loop(elapsed, renderer.get());
+        break;
     }
-    if (closest_t != 1.0) {
-      process_collision(b, delta_position, closest_t, closest_normal);
-      if (closest_block != nullptr) { --(closest_block->durability); }
-    }
-
-    auto new_end = std::remove_if(std::begin(blocks), std::end(blocks), [](const block& b) { return b.durability == 0; });
-    blocks.erase(new_end, std::end(blocks));
-
-    SDL_SetRenderDrawColor(renderer.get(), 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer.get());
-    SDL_SetRenderDrawColor(renderer.get(), 0x00, 0x00, 0xBB, SDL_ALPHA_OPAQUE);
-    SDL_RenderFillRect(renderer.get(), &game_area);
-
-    for (auto&& block : blocks) { block.draw(renderer.get()); }
-
-    player.draw(renderer.get());
-    b.draw(renderer.get());
-
-    SDL_RenderPresent(renderer.get());
   }
+
   SDL_Quit();
   return 0;
 }
